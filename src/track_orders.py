b1718912c5c05bfafd37c010217b79677c4ff0e7
Zeca Costa <jccostaso@gmail.com>
2021-08-09 15:53:51 -0300

    atualizando projeto

--- src/track_orders.py
+++ src/track_orders.py
@@ -7,15 +7,24 @@ class TrackOrders:
 
     def get_most_ordered_dish_per_costumer(self, costumer):
         pass
+        # get_most_ordered_dish_per_costumer('maria')
 
     def get_never_ordered_per_costumer(self, costumer):
         pass
 
+        # get_never_ordered_per_costumer('joao'))
+
     def get_days_never_visited_per_costumer(self, costumer):
         pass
 
+        # get_days_never_visited_per_costumer('joao'))
     def get_busiest_day(self):
         pass
 
     def get_least_busy_day(self):
         pass
+
+    def get_order_frequency_per_costumer(self, costumer, order):
+        pass
+
+        # get_order_frequency_per_costumer('arnaldo', 'hamburguer'))
