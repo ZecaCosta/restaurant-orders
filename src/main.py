5a61487a92559c1be6756bed2ec4be73e19d65ca
Vitor Buxbaum Orlandi <vitorbuxbaum@gmail.com>
2021-08-09 09:13:18 -0300

    Atualiza nome de método chamado

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ from track_orders import TrackOrders
 
 def print_info(tracker, control):
     print(tracker.get_most_ordered_dish_per_costumer('maria'))
-    print(tracker.get_order_frequency_per_costumer('arnaldo', 'hamburguer'))
+    print(tracker.get_dish_quantity_per_costumer('arnaldo', 'hamburguer'))
     print(tracker.get_never_ordered_per_costumer('joao'))
     print(tracker.get_days_never_visited_per_costumer('joao'))
     print(control.get_quantities_to_buy())
